db6585d4d1a79f632606a22a02fcacc3ff9af407
Katy Jiang <katyjiang@Katys-MBP.attlocal.net>
2021-09-27 14:39:47 -0700

    Integration updated

---
+++ frontend/src/components/Home.vue
@@ -0,0 +1,195 @@
+<template>
+  <div class="container">
+    <Nav/>
+    <div class="search-wrapper">
+        <form v-on:submit.prevent="submitForm">
+
+            <input type="text" v-model="itemName" placeholder="Search everithing at Octopus"/>
+            <input type="text" v-model="latitude" placeholder="Latitude" />
+            <input type="text" v-model="longitude" placeholder="Longitude" />
+            <div><button @click="locateMe">Get my location</button></div>
+            <div v-if="errorStr">
+                Sorry, but the following error
+                occurred: {{errorStr}}
+            </div>
+            
+            <div v-if="gettingLocation">
+                <i>Getting your location...</i>
+            </div>
+            
+            <!-- <div v-if="location">
+                Your current location is {{ location.coords.latitude }}, {{ location.coords.longitude}}
+            </div> -->
+            <div><button @click="submit">Search</button></div>
+        </form>
+    </div>
+
+    <h1 class="wgr"><img src='../assets/walgreens.jpg' width="50" height="50" class="image"/>Walgreens</h1>
+    
+    <div class="test-cards-container">
+        <ProductItem 
+            v-for="item in walgreensList"
+            v-bind:href="item.name"
+            :key="item.id"
+            :item="item"   
+        />
+    </div>
+
+    <h1 class="tgt"><img src='../assets/target.jpg'  width="50" height="50" class="image"/>Target</h1>   
+    
+
+    <div class="test-cards-container">
+        <ProductItem 
+            v-for="item in targetList"
+            v-bind:href="item.name"
+            :key="item.id"
+            :item="item"   
+        />
+    </div>
+
+    <h1 class="wmr"><img src='../assets/walmart.jpg'  width="50" height="50" class="image"/>Walmart</h1>    
+    
+
+    <div class="test-cards-container">
+        <ProductItem 
+            v-for="item in walmartList"
+            v-bind:href="item.name"
+            :key="item.id"
+            :item="item"   
+        />
+    </div>
+  </div>
+</template>
+
+<script>
+import Nav from './Nav.vue'
+import ProductItem from './product/ProductItem.vue'
+import { mapGetters } from 'vuex'
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+        test: null,
+        itemName: '',
+        location:null,
+        gettingLocation: false,
+        errorStr:null,
+        latitude:null,
+        longitude:null,
+        }
+    },
+    components: {
+        Nav,
+        ProductItem
+    },
+    computed: {
+        ...mapGetters(['walmart', 'target', 'walgreens']),
+        walmartList() {
+            return this.walmart.slice(0,6)
+        },
+        targetList() {
+            return this.target.slice(0,6)
+        },
+        walgreensList() {
+            return this.walgreens.slice(0,6)
+        }
+    },
+    methods: {
+        submit(){
+            axios.post("http://localhost:8080/search", {
+                itemName: this.itemName,
+                latitude: this.latitude,
+                longitude: this.longitude,
+            })
+            .then((response) => {
+                this.thumbnailUrl = response.data.screenshot;
+            })
+            .catch((error) => {
+                window.alert(`The API returned an error: ${error}`);
+            })
+
+            console.log(this.itemName,this.latitude,this.longitude)
+        },
+        async getLocation() {
+      
+        return new Promise((resolve, reject) => {
+
+            if(!("geolocation" in navigator)) {
+            reject(new Error('Geolocation is not available.'));
+            }
+
+            navigator.geolocation.getCurrentPosition(pos => {
+            resolve(pos);
+            }, err => {
+            reject(err);
+            });
+
+        });
+        },
+        async locateMe() {
+
+        this.gettingLocation = true;
+        try {
+            this.gettingLocation = false;
+            this.location = await this.getLocation();
+            this.latitude = this.location.coords.latitude;
+            this.longitude = this.location.coords.longitude;
+        } catch(e) {
+            this.gettingLocation = false;
+            this.errorStr = e.message;
+        }
+        
+        }
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-wrapper {
+    position: relative;
+    margin-top: 22px;
+    input {
+      padding: 3px;
+      width: 500px;
+      height: 28px;
+      color: rgba(0,0,0,.70);
+      border: 1px solid rgba(0,0,0,.12);
+      transition: .15s all ease-in-out;
+      background: white;
+  
+      &::placeholder{
+        font-size: 12px;
+        color: rgba(10, 10, 10, 0.5);
+        font-weight: 800;
+      }
+    }
+  }
+  .image{
+      vertical-align:middle;
+  }
+    .wgr{
+        background-color: rgb(56, 47, 102);
+        color: rgb(247, 41, 26);
+        padding: 5px;
+    }
+
+    .tgt{
+        background-color: rgb(196, 16, 16);
+        color: white;
+        padding: 5px;
+
+    }
+    .wmr{
+        background-color: rgb(18, 109, 245);
+        color: white;
+        padding: 5px;
+    }
+  .test-cards-container {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    padding-top: 10px;
+  }
+</style>
