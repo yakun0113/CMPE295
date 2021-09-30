02ab5eddba23463432be8ee98facaa885ad6e997
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2021-09-29 23:49:01 -0700

    Update scraper.py

--- pythonScraping/scraper.py
+++ pythonScraping/scraper.py
@@ -4,20 +4,21 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
 from threading import Thread
-from multiprocessing import Process, Manager
 
 def get_walmart(product_name, option, latitude, longitude):
-    
+
     Walmart = requests.get("https://serpapi.com/search.json?engine=google_maps&q=walmart&ll=%40"+latitude+"%2C"+longitude+"%2C15z&type=search&api_key=" + serpApi_key)
     Walmart = Walmart.json()
     Walmart_url = Walmart["local_results"][0]["website"]
+    product_name = product_name.replace(" ","+")
     Walmart_url = Walmart_url.split("?")[0] + "/search?query=" + product_name
     Walmart_location = Walmart["local_results"][0]["gps_coordinates"]
 
-    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/pythonScraping/chromedriver", options = option)
+    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/CMPE295-main-5/pythonScraping/chromedriver", options = option)
     driver.get(Walmart_url)
     button = driver.find_element_by_xpath('//*[@id="content"]/div[2]/section[2]/div[2]/div/div/div/div/div/div[2]/form/div/div[3]/button')
     button.click()
+
     for i in range(10):
         driver.execute_script("window.scrollBy(0, document.body.scrollHeight/10)")
         time.sleep(0.1)
@@ -36,36 +37,40 @@ def get_target(product_name, option, latitude, longitude):
     Target_url=page["place_results"]["website"]
     Target_location = Target["local_results"][0]["gps_coordinates"]
 
-    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/pythonScraping/chromedriver", options = option)
+    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/CMPE295-main-5/pythonScraping/chromedriver", options = option)
     driver.get(Target_url)
     driver.execute_script("window.scrollBy(0, document.body.scrollHeight/5)")
-    time.sleep(0.2)
     button = driver.find_element_by_xpath("//*[@id='__next']/div[3]/div/div[1]/div[2]/div/div[5]/button[2]")
     button.click()
+    product_name = product_name.replace(" ","+")
     driver.get("https://www.target.com/s?searchTerm="+product_name+"&facetedValue=5zkty")
-    time.sleep(0.2)
+    time.sleep(0.5)
 
     for i in range(15):
         driver.execute_script("window.scrollBy(0, document.body.scrollHeight/15)")
         time.sleep(0.1)
+
     target_soup = BeautifulSoup(driver.page_source, 'html.parser')
     driver.quit()
     scrape_target(target_soup, Target_location)
 
 def get_walgreens(product_name, option, latitude, longitude):
-  
+    
     Walgreens = requests.get("https://serpapi.com/search.json?engine=google_maps&q=walgreens&ll=%40"+latitude+"%2C"+longitude+"%2C15z&type=search&api_key=" + serpApi_key)
     Walgreens = Walgreens.json()
     Walgreens_url = Walgreens["local_results"][0]["website"]
     Walgreens_location = Walgreens["local_results"][0]["gps_coordinates"]
     
-    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/pythonScraping/chromedriver", options = option)
-    driver.get(Walgreens_url)
+    driver = webdriver.Chrome(executable_path="/Users/brian80433/Desktop/CMPE295-main-5/pythonScraping/chromedriver", options = option)
+    driver.get(Walgreens_url) 
     search = driver.find_element_by_xpath('//*[@id="shop-placeholder"]')
     search.send_keys(product_name)
     button = driver.find_element_by_xpath('//*[@id="storeServiceDiv"]/div[1]/div[1]/div[3]/div/div/button')
     button.click()
-    time.sleep(0.2)
+    time.sleep(1)
+    pickUp = driver.find_element_by_xpath('//*[@id="IN_STORES"]')
+    pickUp.click()
+    time.sleep(1)
 
     for i in range(15):
         driver.execute_script("window.scrollBy(0, document.body.scrollHeight/15)")
@@ -76,9 +81,11 @@ def get_walgreens(product_name, option, latitude, longitude):
     scrape_walgreens(walgreens_soup, Walgreens_location)
 
 def scrape_walmart(soup, Walmart_location):
+
     p_list = []
     productId = 0
     productInfo = soup.select('ul[class="results-list grid-view"] div[class="Grid-col u-size-6-12 u-size-4-12-s u-size-3-12-m u-size-3-12-l"]')
+    
     for result in productInfo:
         productImage = result.select_one('img[class="tile-img"]')["src"]    
         productName = result.select_one('div[class="tile-title"]').text
@@ -88,11 +95,11 @@ def scrape_walmart(soup, Walmart_location):
         productLink = result.select_one('div[class="tile u-focusTile"] a[class="display-block"]')['href']
 
         if productPrice == None:
-            productPrice = "None"
+            productPrice = "Price available in store"
         else:
             productPrice = productPrice.text
         if productRating == None:
-            productRating = "None"
+            productRating = "No rating"
         else:
             productRating = productRating['aria-label']
         productImage = (productImage.split("//"))[1]
@@ -112,7 +119,6 @@ def scrape_walmart(soup, Walmart_location):
                   } 
             p_list.append(product)    
             productId += 1
-
     
     store = {
         "store": "Walmart",
@@ -123,17 +129,28 @@ def scrape_walmart(soup, Walmart_location):
     product_list.append(store)
     
 def scrape_target(soup, Target_location):
+
     p_list = []
     productId = 100
-
     productInfo = soup.select('ul[class="Row-uds8za-0 jBYETz h-padding-t-tight"] li[class="Col-favj32-0 iXmsJV h-padding-a-none h-display-flex"]')
+    
     for result in productInfo:
         productImage = result.select_one('img')["src"]    
         productNameLink = result.select_one('a[class="Link__StyledLink-sc-4b9qcv-0 styles__StyledTitleLink-h3r0um-1 iBIqkb rwewC h-display-block h-text-bold h-text-bs"]')
         productName = productNameLink.text
-        productPrice = result.select_one('div[data-test="current-price"]').span.text
-        productRating = result.select_one('span[class="utils__ScreenReaderOnly-sc-1p6kq06-0 iyDnVM"]').text
+        productPrice = result.select_one('div[data-test="current-price"]').span
+        productRating = result.select_one('span[class="utils__ScreenReaderOnly-sc-1p6kq06-0 iyDnVM"]')
         productLink = "https://www.target.com"+productNameLink["href"]
+
+        if productPrice == None:
+            productPrice = "Price available in store"
+        else:
+            productPrice = productPrice.text
+
+        if productRating == None:
+            productRating = "No rating"
+        else:
+            productRating = productRating.text
 
         product = {
                 "id": productId,
@@ -143,8 +160,10 @@ def scrape_target(soup, Target_location):
                 "rating": productRating,
                 "link": productLink
                   } 
+
         p_list.append(product)    
         productId += 1
+
     store = {
         "store": "Target",
         "location": Target_location,
@@ -153,21 +172,27 @@ def scrape_target(soup, Target_location):
 
     product_list.append(store)
 
-
 def scrape_walgreens(soup, Walgreens_location):
+
     p_list = []
     productId = 200
-
     productInfo = soup.select('div[class="item card card__product in-stores"]')
+
     for result in productInfo:
         productImage = result.select_one('figure[class="product__img"] img')["src"]
         productImage = "https:" + productImage
         productName = result.select_one('div[name="product-title"]').text
-        productPrice = result.select_one('div[class="product__price-contain"] span span span').text
+        productPrice = result.select_one('div[class="product__price-contain"] span span span')
         productRating = result.select_one('span[class="product__rating"] figure img')
         productStatus = result.select_one('div[class="avail-cta"] strong')
         productLink = result.select_one('div[class="product__text"] a')["href"]
         productLink = "https://www.walgreens.com"+productLink
+
+        if productPrice == None:
+            productPrice = "Price available in store"
+        else:
+            productPrice = productPrice.text
+
         if productRating == None:
             productRating = "No Rating"
         else:
@@ -188,6 +213,7 @@ def scrape_walgreens(soup, Walgreens_location):
 
         else: 
             continue
+    
     store = {
         "store": "Walgreens",
         "location": Walgreens_location,
@@ -205,14 +231,13 @@ if __name__ == '__main__':
     product_name = info[0]
     latitude = info[1]
     longitude = info[2]
-        
     product_list = []
-    serpApi_key = "826f229af259ebcfec493907d7b1f5e923f3efd20b8ea6b67417a6659870e7dc"
+    serpApi_key = "dafa5c94164dee0a24cfff9251cd140c253688f5be306031b69bcf4b8ca090b2"
     option = webdriver.ChromeOptions()
     option.add_argument("--headless")
-
+    option.add_argument("--window-size=1920x1080")
     args = (product_name, option, latitude, longitude)
-
+    
     t1 = Thread(target=get_walmart, args= args)
     t2 = Thread(target=get_target, args= args)
     t3 = Thread(target=get_walgreens, args= args)
@@ -227,6 +252,8 @@ if __name__ == '__main__':
     
     with open('products.json', 'w') as outfile:
         json.dump(product_list, outfile, indent=4)
+    
+
     
 
  
