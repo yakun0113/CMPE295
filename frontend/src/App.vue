f1cf59f1c1c6d4e6be884f668a82762919e33e41
brianhsu0603 <56208363+brianhsu0603@users.noreply.github.com>
2021-11-16 09:39:54 -0800

    Update App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,18 +2,17 @@
 <div class="container">
   <Nav/>
   <router-view />
-  <Footer/>
 </div>
 
 </template>
 
 <script>
 import Nav from './components/Nav.vue'
-import Footer from './components/Footer.vue'
+//import Footer from './components/Footer.vue'
 export default {
   components: {
     Nav,
-    Footer,
+    //Footer,
   }
 }
 </script>
